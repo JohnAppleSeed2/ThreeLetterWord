b9e76d4a8e7640b67ca54fa0fa2927d9a7aa38f1
JOHN SANCHEZ <rl200295@rlas-macair-29.local>
2018-11-28 10:23:15 -0600

    Initial Commit

---
+++ ThreeLetterWord/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThreeLetterWord
+//
+//  Created by JOHN SANCHEZ on 11/28/18.
+//  Copyright © 2018 JOHN SANCHEZ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
